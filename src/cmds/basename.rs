db6f897473d9dc28fe13e165da77fef642a57b29
cristivlas <cristi.vlasceanu@gmail.com>
2024-08-10 13:50:30 -0700

    add missing file

---
+++ src/cmds/basename.rs
@@ -0,0 +1,58 @@
+use super::{register_command, Exec, ShellCommand};
+use crate::cmds::flags::CommandFlags;
+use crate::eval::{Scope, Value};
+use std::path::Path;
+use std::rc::Rc;
+
+struct Basename {
+    flags: CommandFlags,
+}
+
+impl Basename {
+    fn new() -> Self {
+        let mut flags = CommandFlags::new();
+        flags.add_flag('?', "help", "Display this help message");
+        Basename { flags }
+    }
+}
+
+impl Exec for Basename {
+    fn exec(&self, _name: &str, args: &Vec<String>, _: &Rc<Scope>) -> Result<Value, String> {
+        let mut flags = self.flags.clone();
+        flags.parse(args)?;
+
+        if flags.is_present("help") {
+            println!("Usage: basename [OPTION]... [NAME]...");
+            println!("Return the base name of each FILE.");
+            println!("\nOptions:");
+            print!("{}", flags.help());
+            return Ok(Value::success());
+        }
+
+        if args.is_empty() {
+            return Err("No arguments provided.".to_string());
+        }
+
+        for arg in args {
+            let path = Path::new(arg);
+            let base = path
+                .file_name()
+                .ok_or_else(|| "Failed to get file name".to_string())?
+                .to_string_lossy();
+            println!("{}", base);
+        }
+        Ok(Value::success())
+    }
+
+    fn is_external(&self) -> bool {
+        false
+    }
+}
+
+#[ctor::ctor]
+fn register() {
+    register_command(ShellCommand {
+        name: "basename".to_string(),
+        inner: Rc::new(Basename::new()),
+    });
+}
