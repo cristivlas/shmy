6bc0d047a385594b57b5065f6ef29a5f3305c0ac
cristivlas <cristi.vlasceanu@gmail.com>
2024-09-13 22:42:09 -0700

    add filtering and sorting to 'ps' command

--- src/cmds/ps.rs
+++ src/cmds/ps.rs
@@ -1,8 +1,13 @@
 use super::{flags::CommandFlags, register_command, Exec, ShellCommand};
 use crate::{eval::Value, scope::Scope};
+use std::any::Any;
+use std::cmp::{Ord, Ordering, PartialOrd};
+use std::ffi::OsString;
 use std::fmt;
 use std::sync::Arc;
 use sysinfo::{Pid, Process, System, Uid, Users};
+
+const MAX_STR_WIDTH: usize = 32;
 
 trait Filter {
     fn apply<'a>(&self, proc: &'a Process) -> Option<&'a Process>;
@@ -12,6 +17,7 @@ trait Filter {
 type Fmt = Box<dyn Fn(&mut fmt::Formatter<'_>, &dyn fmt::Display) -> fmt::Result>;
 
 trait Field {
+    fn as_any(&self) -> &dyn Any;
     fn to_string(&self, fmt: &Fmt) -> String;
 }
 
@@ -21,7 +27,8 @@ where
     G: Fn(&Process) -> T,
     T: Field,
 {
-    name: String,
+    name: &'static str,
+    header: &'static str,
     fmt: Fmt,
     getter: G,
 }
@@ -31,9 +38,10 @@ where
     G: Fn(&Process) -> T,
     T: Field + 'static,
 {
-    fn new(name: &str, fmt: Fmt, getter: G) -> Self {
+    fn new(name: &'static str, header: &'static str, fmt: Fmt, getter: G) -> Self {
         Self {
-            name: name.to_string(),
+            name,
+            header,
             fmt,
             getter,
         }
@@ -41,42 +49,76 @@ where
 }
 
 struct Header<'a> {
-    col: &'a dyn ViewColumn,
+    col: &'a Box<dyn ViewColumn>,
+}
+
+impl<'a> Header<'a> {
+    fn new(col: &'a Box<dyn ViewColumn>) -> Self {
+        Self { col }
+    }
 }
 
 impl<'a> fmt::Display for Header<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.col.fmt(f, &self.col.name())
+        self.col.fmt(f, &self.col.header())
     }
 }
 
 /// The interface for a column in the processes view.
 trait ViewColumn {
+    /// Order two processes based on this column.
+    fn cmp(&self, lhs: &Process, rhs: &Process) -> Ordering;
+
+    /// Format this column for printing
     fn fmt(&self, f: &mut fmt::Formatter<'_>, d: &dyn fmt::Display) -> fmt::Result;
-    fn field(&self, proc: &Process) -> String;
-    fn header(&self) -> Header;
-    fn name(&self) -> &str;
+
+    /// Extract field that corresponds to this column from process.
+    fn field(&self, proc: &Process) -> Box<dyn Field>;
+
+    /// Extract field and format at string, for printing.
+    fn field_as_string(&self, proc: &Process) -> String;
+
+    /// Get the printable column header.
+    fn header(&self) -> &str;
+
+    /// Get the column's name.
+    fn name(&self) -> &'static str;
 }
 
 impl<G, T> ViewColumn for Column<G, T>
 where
     G: Fn(&Process) -> T,
-    T: Field + 'static,
+    T: Field + Ord + 'static,
 {
+    fn cmp(&self, lhs: &Process, rhs: &Process) -> Ordering {
+        if let (Some(lhs), Some(rhs)) = (
+            self.field(lhs).as_any().downcast_ref::<T>(),
+            self.field(rhs).as_any().downcast_ref::<T>(),
+        ) {
+            lhs.cmp(rhs)
+        } else {
+            // Types don't match or can't be compared? Fall back to string comparison.
+            self.field_as_string(lhs).cmp(&self.field_as_string(rhs))
+        }
+    }
+
     fn fmt(&self, f: &mut fmt::Formatter<'_>, d: &dyn fmt::Display) -> fmt::Result {
         (self.fmt)(f, d)
     }
 
-    fn field(&self, proc: &Process) -> String {
-        let field = Box::new((self.getter)(proc));
-        field.to_string(&self.fmt)
+    fn field(&self, proc: &Process) -> Box<dyn Field> {
+        Box::new((self.getter)(proc))
     }
 
-    fn header(&self) -> Header<'_> {
-        Header { col: self }
+    fn field_as_string(&self, proc: &Process) -> String {
+        self.field(proc).to_string(&self.fmt)
     }
 
-    fn name(&self) -> &str {
+    fn header(&self) -> &str {
+        &self.header
+    }
+
+    fn name(&self) -> &'static str {
         &self.name
     }
 }
@@ -103,22 +145,73 @@ impl<'a, T: fmt::Display> fmt::Display for Helper<'a, T> {
     }
 }
 
-impl Field for f32 {
+#[derive(PartialEq, PartialOrd)]
+struct F32(f32);
+
+impl Eq for F32 {}
+
+impl Ord for F32 {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.partial_cmp(&other.0).unwrap_or(Ordering::Less)
+    }
+}
+
+impl Field for F32 {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
     fn to_string(&self, fmt: &Fmt) -> String {
-        Helper::new(format!("{:10.2}", self), fmt).to_string()
+        Helper::new(format!("{:10.2}", self.0), fmt).to_string()
     }
 }
 
 impl Field for Pid {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
     fn to_string(&self, fmt: &Fmt) -> String {
         Helper::new(self.as_u32(), fmt).to_string()
     }
 }
 
-impl Field for String {
+impl Field for Option<Pid> {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
     fn to_string(&self, fmt: &Fmt) -> String {
-        let s = if self.len() > 30 { &self[..30] } else { &self };
+        match self {
+            Some(pid) => Helper::new(pid.as_u32(), fmt).to_string(),
+            None => Helper::new(0, fmt).to_string(),
+        }
+    }
+}
+
+impl Field for String {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn to_string(&self, fmt: &Fmt) -> String {
+        let s = if self.len() > MAX_STR_WIDTH {
+            &self[..MAX_STR_WIDTH]
+        } else {
+            &self
+        };
         Helper::new(s, fmt).to_string()
+    }
+}
+
+/// OsString is used for showing the command that started a process.
+impl Field for OsString {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn to_string(&self, fmt: &Fmt) -> String {
+        Helper::new(self.to_string_lossy(), fmt).to_string()
     }
 }
 
@@ -133,6 +226,10 @@ fn get_users() -> &'static Users {
 }
 
 impl Field for Option<Uid> {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
     fn to_string(&self, fmt: &Fmt) -> String {
         match self {
             Some(uid) => match get_users().iter().find(|user| user.id() == uid) {
@@ -187,6 +284,7 @@ impl Filter for UserProc {
 struct View {
     columns: Vec<Box<dyn ViewColumn>>,
     filters: Vec<Box<dyn Filter>>,
+    sort_keys: Vec<(&'static str, bool)>, // (name, reverse)
     system: System,
 }
 
@@ -198,24 +296,9 @@ impl View {
         Self {
             columns: vec![],
             filters: vec![],
+            sort_keys: vec![],
             system,
         }
-    }
-
-    fn with_default_view() -> Self {
-        let mut view = View::new();
-
-        view.columns.push(Self::pid_column());
-        view.columns.push(Self::user_column());
-        view.columns.push(Self::name_column());
-        view.columns.push(Self::cpu_usage_column());
-        view.columns.push(Self::mem_usage_column());
-
-        view
-    }
-
-    fn with_all_procs() -> Self {
-        Self::with_default_view()
     }
 
     fn list_processes(&self) -> Result<(), String> {
@@ -225,52 +308,127 @@ impl View {
             if !header.is_empty() {
                 header.push_str("  ");
             }
-            header.push_str(&col.header().to_string());
+            header.push_str(&Header::new(col).to_string());
         }
         my_println!("{}", header)?;
 
-        for (_, proc) in self.system.processes() {
-            if self
-                .filters
-                .iter()
-                .any(|filter| filter.apply(proc).is_none())
-            {
-                continue;
+        let mut processes: Vec<_> = self
+            .system
+            .processes()
+            .iter()
+            .map(|(_, p)| p)
+            .filter_map(|p| {
+                self.filters
+                    .iter()
+                    .fold(Some(p), |p, f| p.and_then(|p| f.apply(p)))
+            })
+            .collect();
+
+        // Iterate over the sort keys in reverse, to ensure stable sort.
+        for (k, reverse) in self.sort_keys.iter().rev() {
+            if let Some(col) = self.columns.iter().find(|col| col.name() == *k) {
+                processes.sort_by(|lhs, rhs| {
+                    let ord = col.cmp(lhs, rhs);
+                    if *reverse {
+                        ord.reverse()
+                    } else {
+                        ord
+                    }
+                });
             }
+        }
+
+        for p in processes {
             for col in &self.columns {
-                my_print!("{}  ", col.field(proc))?;
+                my_print!("{}  ", col.field_as_string(p))?;
             }
             my_println!()?;
         }
         Ok(())
     }
 
+    fn parse_sort_spec(&mut self, sort_spec: &str) -> Result<(), String> {
+        let mut seen = std::collections::HashSet::new();
+
+        for spec in sort_spec.split(',') {
+            let (name, reverse) = match spec.trim() {
+                s if s.starts_with('-') => (&s[1..], true),
+                s if s.starts_with('+') => (&s[1..], false),
+                s => (s, false), // default ascending
+            };
+
+            // Find the column by name and get the reference to the static `name`
+            if let Some(col) = self.columns.iter().find(|col| col.name() == name) {
+                let col_name = col.name(); // 'static reference
+                if !seen.insert(col_name) {
+                    return Err(format!("Duplicate sort key: {}", col_name));
+                }
+                self.sort_keys.push((col_name, reverse));
+            } else {
+                return Err(format!("Invalid sort key: {}", name));
+            }
+        }
+
+        Ok(())
+    }
+
+    //
+    // Factory methods for ViewColumn-s
+    //
     fn cpu_usage_column() -> Box<dyn ViewColumn> {
         Box::new(Column::new(
+            "cpu",
             "CPU%",
             Box::new(|f, d| write!(f, "{:>10}", d)),
-            Box::new(Process::cpu_usage),
+            Box::new(|proc: &Process| F32(proc.cpu_usage())),
+        ))
+    }
+
+    fn cmd_column() -> Box<dyn ViewColumn> {
+        Box::new(Column::new(
+            "cmd",
+            "CMD",
+            Box::new(|f, d| write!(f, "{:<}", d)),
+            Box::new(|proc: &Process| {
+                proc.cmd()
+                    .iter()
+                    .map(|s| s.to_owned())
+                    .collect::<Vec<_>>()
+                    .join(&OsString::from(" "))
+            }),
         ))
     }
 
     fn mem_usage_column() -> Box<dyn ViewColumn> {
         Box::new(Column::new(
+            "mem",
             "MEM (MB)",
             Box::new(|f, d| write!(f, "{:>10}", d)),
-            Box::new(|p: &Process| p.memory() as f32 / 1024.0 / 1024.0),
+            Box::new(|proc: &Process| F32(proc.memory() as f32 / 1024.0 / 1024.0)),
         ))
     }
 
     fn name_column() -> Box<dyn ViewColumn> {
         Box::new(Column::new(
+            "name",
             "NAME",
-            Box::new(|f, d| write!(f, "{:<30}", d)),
+            Box::new(|f, d| write!(f, "{:<MAX_STR_WIDTH$}", d)),
             Box::new(|p: &Process| p.name().to_string_lossy().to_string()),
+        ))
+    }
+
+    fn parent_pid_column() -> Box<dyn ViewColumn> {
+        Box::new(Column::new(
+            "ppid",
+            "PPID",
+            Box::new(|f, d| write!(f, "{:>8}", d)),
+            Box::new(Process::parent),
         ))
     }
 
     fn pid_column() -> Box<dyn ViewColumn> {
         Box::new(Column::new(
+            "pid",
             "PID",
             Box::new(|f, d| write!(f, "{:>8}", d)),
             Box::new(Process::pid),
@@ -279,19 +437,11 @@ impl View {
 
     fn user_column() -> Box<dyn ViewColumn> {
         Box::new(Column::new(
+            "uid",
             "USER",
             Box::new(|f, d| write!(f, "{:>10}", d)),
             Box::new(|p: &Process| p.user_id().map(|u| u.clone())),
         ))
-    }
-}
-
-impl Default for View {
-    fn default() -> Self {
-        let mut view = Self::with_default_view();
-        view.filters.push(Box::new(UserProc::new(&view.system)));
-
-        view
     }
 }
 
@@ -308,6 +458,8 @@ impl ProcStatus {
             "all",
             "List all processes, not just processes belonging to the current user",
         );
+        flags.add_flag('l', "long", "Long format");
+        flags.add_option('s', "sort", "Specify sorting order");
 
         Self { flags }
     }
@@ -316,7 +468,9 @@ impl ProcStatus {
 impl Exec for ProcStatus {
     fn exec(&self, _name: &str, args: &Vec<String>, scope: &Arc<Scope>) -> Result<Value, String> {
         let mut flags = self.flags.clone();
-        let _ = flags.parse(scope, args)?;
+
+        // Use forgiving, non-error checking parsin here, for compat with ps -efl, ps -afx etc.
+        let _ = flags.parse_all(scope, args);
 
         if flags.is_present("help") {
             println!("Usage: ps [OPTIONS]");
@@ -326,11 +480,27 @@ impl Exec for ProcStatus {
             return Ok(Value::success());
         }
 
-        let view = if flags.is_present("all") {
-            View::with_all_procs()
-        } else {
-            View::default()
-        };
+        let mut view = View::new();
+
+        view.columns.push(View::user_column());
+        view.columns.push(View::pid_column());
+        view.columns.push(View::parent_pid_column());
+        view.columns.push(View::name_column());
+        view.columns.push(View::cpu_usage_column());
+        view.columns.push(View::mem_usage_column());
+
+        if flags.is_present("long") {
+            view.columns.push(View::cmd_column());
+        }
+
+        if let Some(sort_spec) = flags.option("sort") {
+            view.parse_sort_spec(sort_spec)?;
+        }
+
+        if !flags.is_present("all") {
+            view.filters.push(Box::new(UserProc::new(&view.system)));
+        }
+
         view.list_processes()?;
 
         Ok(Value::success())
