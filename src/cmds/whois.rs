ae09a9bb83ce959187d0a08d43fc01b4920f9b14
cristivlas <cristi.vlasceanu@gmail.com>
2024-09-30 23:08:49 -0700

    add timeout optional arg to whois

--- src/cmds/whois.rs
+++ src/cmds/whois.rs
@@ -19,7 +19,9 @@ struct Whois {
 impl Whois {
     fn new() -> Self {
         let mut flags = CommandFlags::with_help();
-        flags.add_value('h', "host", "server", "whois server");
+        flags.add_value('h', "host", "server", "Whois server");
+        flags.add_value('t', "timeout", "seconds", "Read/write timeout (default: 10 sec)");
+
         Self { flags }
     }
 
@@ -30,10 +32,14 @@ impl Whois {
         }
     }
 
-    fn query_whois(server: &str, ip: &str) -> io::Result<io::Lines<BufReader<TcpStream>>> {
+    fn query_whois(
+        server: &str,
+        ip: &str,
+        timeout: u64,
+    ) -> io::Result<io::Lines<BufReader<TcpStream>>> {
         let mut stream = TcpStream::connect((server, 43))?;
-        stream.set_read_timeout(Some(Duration::new(10, 0)))?;
-        stream.set_write_timeout(Some(Duration::new(10, 0)))?;
+        stream.set_read_timeout(Some(Duration::new(timeout, 0)))?;
+        stream.set_write_timeout(Some(Duration::new(timeout, 0)))?;
 
         let query = format!("{}\r\n", ip);
         stream.write_all(query.as_bytes())?;
@@ -42,12 +48,13 @@ impl Whois {
         Ok(reader.lines())
     }
 
-    fn whois(args: &[String], server: Option<&str>) -> Result<Value, String> {
+    fn whois(args: &[String], server: Option<&str>, timeout: u64) -> Result<Value, String> {
         let ip_str = &args[0];
         match ip_str.parse::<IpAddr>() {
             Ok(ip) => {
                 let whois_server = server.unwrap_or(Whois::get_whois_server(&ip));
-                let lines = Whois::query_whois(&whois_server, ip_str).map_err(|e| e.to_string())?;
+                let lines = Whois::query_whois(&whois_server, ip_str, timeout)
+                    .map_err(|e| e.to_string())?;
 
                 for line in lines {
                     my_println!("{}", line.map_err(|e| e.to_string())?)?;
@@ -80,8 +87,14 @@ impl Exec for Whois {
             return Err("Missing IP address".to_string());
         }
 
+        let timeout = flags
+            .value("timeout")
+            .unwrap_or("10")
+            .parse::<u64>()
+            .map_err(|e| format!("Error parsing timeout value: {}", e))?;
+
         _ = execute!(stdout(), DisableLineWrap);
-        let result = Self::whois(&whois_args, flags.value("host"));
+        let result = Self::whois(&whois_args, flags.value("host"), timeout);
         _ = execute!(stdout(), EnableLineWrap);
 
         Ok(result.map_err(|e| format_error(scope, &whois_args[0], &args, e))?)
